14978178c0c4ceaee1f0195aa4ee2b873c62531c
21343054 <112170832+21343054@users.noreply.github.com>
2022-10-20 20:01:17 +0700

    Add files via upload

---
+++ Jobsheet 8/src/Latihan/ArraySample3.java
@@ -0,0 +1,22 @@
+package Latihan;
+/*
+ * 
+ * Nama : Maulana Hafizul Haq
+ * Nim  : 21343054
+ * 
+ */
+public class ArraySample3 {
+    public static void main(String[] args) {
+        
+        //String array 4 bars x 2 kolom
+        String[][] dogs = {{"Terry","brown"},
+         {"Kristin","white"},
+        {"Toby","gray"},
+        {"Fido","Black"}, };
+                
+        System.out.print(dogs[0][0]);
+                
+
+    }
+    
+}
